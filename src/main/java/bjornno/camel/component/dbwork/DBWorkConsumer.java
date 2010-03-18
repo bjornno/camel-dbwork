89584075ca040dd38e0c0616210ce327471d13e3
Bjrn Nordlund <bjornno@gmail.com>
2010-03-18 19:14:15 +0100

    initial sources

---
+++ src/main/java/bjornno/camel/component/dbwork/DBWorkConsumer.java
@@ -0,0 +1,76 @@
+package bjornno.camel.component.dbwork;
+
+import org.apache.camel.ExchangePattern;
+import org.apache.camel.Message;
+import org.apache.camel.Processor;
+import org.apache.camel.impl.DefaultExchange;
+import org.apache.camel.impl.DefaultMessage;
+import org.apache.camel.impl.ScheduledPollConsumer;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+
+public class DBWorkConsumer extends ScheduledPollConsumer {
+    private DBWorkEndpoint endpoint;
+    private JdbcTemplate jdbcTemplate;
+    private final ScheduledThreadPoolExecutor executor;
+    private final int concurrentConsumers = 6;
+
+    public DBWorkConsumer(DBWorkEndpoint endpoint, Processor processor, JdbcTemplate jdbcTemplate) {
+        super(endpoint, processor);
+
+        this.endpoint = endpoint;
+        this.jdbcTemplate = jdbcTemplate;
+        this.executor = new ScheduledThreadPoolExecutor(this.concurrentConsumers);
+    }
+
+    @Override
+    protected void poll() throws Exception {
+        List workids = jdbcTemplate.queryForList("select workid from dbwork where queuename = '"+endpoint.getRemaining()+"' and status = 0", String.class);
+
+
+        for (Iterator iterator = workids.iterator(); iterator.hasNext();) {
+            String workid = (String) iterator.next();
+            Task worker = new Task(endpoint, this.getProcessor(), workid, jdbcTemplate);
+            executor.execute(worker);
+            //executor.scheduleWithFixedDelay(worker, 0, 1, TimeUnit.NANOSECONDS);
+        }
+    }
+
+    @Override
+    protected void doStop() throws Exception {
+        executor.shutdown();
+    }
+}
+
+class Task implements Runnable {
+    private DBWorkEndpoint endpoint;
+    private final Processor processor;
+    private final String workId;
+    private JdbcTemplate jdbcTemplate;
+
+    public Task(DBWorkEndpoint endpoint, Processor processor, String workId, JdbcTemplate jdbcTemplate) throws Exception {
+        this.endpoint = endpoint;
+        this.processor = processor;
+        this.workId = workId;
+        this.jdbcTemplate = jdbcTemplate;
+
+    }
+
+    public void run() {
+        DefaultExchange exchange = (DefaultExchange) endpoint.createExchange(ExchangePattern.InOut);
+        Message message = new DefaultMessage();
+        String body = jdbcTemplate.queryForObject("select text from resources where id = '"+workId+"'", String.class);
+        message.setBody(body);
+        exchange.setIn(message);
+        try {
+            processor.process(exchange);
+        } catch (Exception e) {
+            jdbcTemplate.update("update dbwork set status = 1 where workid = '" + workId + "'");
+        }
+        jdbcTemplate.update("update dbwork set status = 2 where workid = '" + workId + "'");
+    }
+
+}
---
+++ src/main/java/bjornno/camel/component/dbwork/DBWorkProducer.java
@@ -0,0 +1,21 @@
+package bjornno.camel.component.dbwork;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.impl.DefaultProducer;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class DBWorkProducer extends DefaultProducer {
+    private String queueName;
+    private JdbcTemplate jdbcTemplate;
+
+    public DBWorkProducer(DBWorkEndpoint endpoint, String queueName, JdbcTemplate jdbcTemplate) {
+        super(endpoint);
+        this.jdbcTemplate = jdbcTemplate;
+        this.queueName = queueName;
+    }
+
+    public void process(final Exchange exchange) throws Exception {
+        jdbcTemplate.execute("insert into dbwork(id, workid, queuename) values('"+exchange.getExchangeId()+"', '"+exchange.getExchangeId()+"','"+queueName+"')");   
+    }
+
+}
